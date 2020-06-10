c62ae3475ea11c3fa1f461e7f1f15539b1a85099
Daniel Nordquist <dnordquist42@gmail.com>
2020-06-10 03:43:27 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
-def say_hello (rubyprogrammer) = ("Hello Ruby Programmer!")
+def say_hello (rubyprogrammer = "Hello Ruby Programmer!")
 puts "Hello Kent Beck!"
 end
